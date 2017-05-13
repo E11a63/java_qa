63e14ef23e79197fea3ad008bdf534a1535244bf
E.Voronova <light-77@bk.ru>
2017-05-13 21:07:12 +0300

    Черновик лекция 1, часть 1

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Dmitrij on 13.05.2017.
+ */
+public class Square {
+  public double l;
+
+  public Square (double l) {
+    this.l=l;
+  }
+  public  double area () {
+    return this.l * this.l;
+  }
+
+
+}
