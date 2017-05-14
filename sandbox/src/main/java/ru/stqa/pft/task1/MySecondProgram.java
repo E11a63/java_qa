ac739f62dfc0926e760fc9de35da57c426247579
E.Voronova <light-77@bk.ru>
2017-05-14 12:45:28 +0300

    Задание 2: часть 4

--- sandbox/src/main/java/ru/stqa/pft/task1/MySecondProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/task1/MySecondProgram.java
@@ -4,19 +4,12 @@ package ru.stqa.pft.task1;
 public class MySecondProgram {
   public static void main (String[] args){
 
-    Point p1 = new Point();
-    p1.x=10;
-    p1.y=4;
-    Point p2 = new Point();
-    p2.x=10;
-    p2.y=5;
-    System.out.println ("Расстояние между точками = " + distance (p1, p2) );
+    Point p1 = new Point(10, 4, 10, 5);
+    System.out.println ("Расстояние между точками = " + p1.metodx());
 
   }
 
-  public static double distance (Point p1, Point p2){
-    return Math.sqrt(((p2.x - p1.x)*(p2.x - p1.x))+ ((p2.y-p1.y)*(p2.y-p1.y)));
-  }
 
 
   }
+
--- sandbox/src/main/java/ru/stqa/pft/task1/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/task1/Point.java
@@ -4,16 +4,22 @@ package ru.stqa.pft.task1;
 public class Point {
 public double x;
 public double y;
+public double z;
+public double w;
 
 
-//public Point (double x, double y) {
-//this.x = x;
-//this.y = y;
-//}
-//  public double metodx() {
-//    return Math.sqrt(((this.x - this.x)*(this.x - this.x))+ ((this.y-this.y)*(this.y-this.y)));
-//  }
-//
+
+public Point (double x, double y, double z, double w) {
+this.x = x;
+this.y = y;
+this.z = z;
+this.w = w;
+
+}
+  public double metodx() {
+    return Math.sqrt (((this.z - this.x)*(this.z - this.x))+((this.w-this.y)*(this.w-this.y)));
+  }
+
 }
 
 
