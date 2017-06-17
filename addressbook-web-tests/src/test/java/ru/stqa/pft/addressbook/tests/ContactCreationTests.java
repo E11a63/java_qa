62dab0a35dfae54d646465cb70c5abf5ddaa94d0
E.Voronova <light-77@bk.ru>
2017-06-17 19:27:12 +0300

    Добавлены датапровайдеры для групп и контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,19 +1,35 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.ContactsData;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestsBase {
 
-  @Test
-  public void testContactCreation() {
+  @DataProvider
+  public Iterator<Object[]> validContacts() {
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[]{new ContactsData().withName("ДатаПровайдер1").withLname("ДатаПровайдеров1").withMname("ДатаПровайдерович1")});
+    list.add(new Object[]{new ContactsData().withName("ДатаПровайдер2").withLname("ДатаПровайдеров2").withMname("ДатаПровайдерович2")});
+    list.add(new Object[]{new ContactsData().withName("ДатаПровайдер3").withLname("ДатаПровайдеров3").withMname("ДатаПровайдерович3")});
+
+    return list.iterator();
+
+
+  }
+
+  @Test(dataProvider = "validContacts")
+  public void testContactCreation(ContactsData contact) {
     app.goTo().HomePage(app);
     Contacts before = app.contact().all();
     app.searchForm();
@@ -23,14 +39,11 @@ public class ContactCreationTests extends TestsBase {
     }
     app.goTo().HomePage(app);
     File photo = new File("src/test/resources/kat.jpg");
-    ContactsData contact = new ContactsData()
-            .withName("Первый").withMname("Первович").withLname("Первов").withGroup("name").withPhoto(photo);
     app.contact().create(contact);
     app.goTo().HomePage(app);
-    assertThat(app.group().count(), equalTo(before.size()+1 ));
-    Contacts after = app.contact().all();
     assertThat(app.contact().count(), equalTo(before.size()+1));
-    //assertThat(before, equalTo(after.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
+    Contacts after = app.contact().all();
+    assertThat(after, equalTo(before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
 
   }
   @Test (enabled = false)
