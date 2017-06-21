0e918efe8bcd2f22c3a79cee09f0f8e58c06ffd0
E.Voronova <light-77@bk.ru>
2017-06-21 21:23:52 +0300

    Добавлено логирование

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -2,7 +2,6 @@ package ru.stqa.pft.addressbook.tests;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.Contacts;
@@ -23,12 +22,13 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestsBase {
 
+
   @DataProvider
   public Iterator<Object[]> validContactsFromCsv() throws IOException {
     List<Object[]> list = new ArrayList<Object[]>();
     BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
-    String line  = reader.readLine();
-    while (line != null){
+    String line = reader.readLine();
+    while (line != null) {
       String[] split = line.split(";");
       list.add(new Object[]{new ContactsData().withName(split[0])
               /*.withMname(split[1])*/.withLname(split[1]).withGroup(split[2]).withPhoto(new File((split[3])))});
@@ -47,11 +47,13 @@ public class ContactCreationTests extends TestsBase {
       line = reader.readLine();
     }
     Gson gson = new Gson();
-    List<ContactsData> contacts = gson.fromJson(json, new TypeToken<List <ContactsData>>(){}.getType()); //List<GroupData>.class
-    return contacts.stream().map((c) -> new Object[] {c}).collect(Collectors.toList()).iterator();
+    List<ContactsData> contacts = gson.fromJson(json, new TypeToken<List<ContactsData>>() {
+    }.getType()); //List<GroupData>.class
+    return contacts.stream().map((c) -> new Object[]{c}).collect(Collectors.toList()).iterator();
   }
 
   @Test(dataProvider = "validContactsFromJson")
+
   public void testContactCreation(ContactsData contact) {
     app.goTo().HomePage(app);
     Contacts before = app.contact().all();
@@ -65,11 +67,11 @@ public class ContactCreationTests extends TestsBase {
     app.contact().create(contact);
     app.goTo().HomePage(app);
     Contacts after = app.contact().all();
-    assertThat(app.contact().count(), equalTo(before.size()+1));
+    assertThat(app.contact().count(), equalTo(before.size() + 1));
     assertThat(after, equalTo(before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
-
   }
-  @Test (enabled = false)
+
+  @Test(enabled = false)
   public void testBadContactCreation() {
     app.goTo().HomePage(app);
     Contacts before = app.contact().all();
